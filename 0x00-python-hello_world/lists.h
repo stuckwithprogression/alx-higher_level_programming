3ec8ad468ea52753701ee1fff9b7040177b2e7f0
stuckwithprogression <chukwunwemichael@gmail.com>
2023-10-02 14:50:15 -0700

    Pycodestyle Tripping

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @a: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+typedef struct listint_s
+{
+	int a;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int a);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
