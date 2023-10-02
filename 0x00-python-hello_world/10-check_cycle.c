0a4bd47c04bb1b7d6c4eceb0b7d83ae64bf6da52
stuckwithprogression <chukwunwemichael@gmail.com>
2023-10-02 15:00:16 -0700

    correcto

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,10 +2,10 @@
 
 /**
  * check_cycle - checks if a linked list contains a cycle
- * @list: exact linked list to check
+ * @list: linked list to check
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
-*/
+ */
 
 int check_cycle(listint_t *list)
 {
