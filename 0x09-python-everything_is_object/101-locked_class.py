fa06f579d4b03aec9c1c9ffa8f07f29acb70443c
stuckwithprogression <chukwunwemichael@gmail.com>
2023-10-31 13:24:20 -0700

    Fixing error with 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Defines a locked class."""
+'''
+    locked class
+'''
 
 
 class LockedClass:
-    """
-    Prevent the user from instantiating new LockedClass attributes
-    for anything but attributes called 'first_name'.
-    """
+    '''
+        prevents a user from dynamically creating new instance attributes,
+        except if the new instance attribute is called first_name
+    '''
 
     __slots__ = ["first_name"]
-
