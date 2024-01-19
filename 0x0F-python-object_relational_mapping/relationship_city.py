1ac53c16a676f4cfb6861488a1e0f4fa4169e0f5
stuckwithprogression <chukwunwemichael@gmail.com>
2024-01-19 05:15:56 -0800

    Changes

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" Module that contains class definition of City and an instance Base """
+"""class definition of a State and an instance base
+"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from relationship_state import Base
 
 
 class City(Base):
-    """ City class representing the cities table """
+    """inherits from Base
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-""" Module that contains class definition of State and an instance Base """
+"""class definition of a State and an instance base
+"""
 
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+Base = declarative_base()
 
 
 class State(Base):
-    """ State class representing the states table """
+    """inherits from Base
+    """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    cities = relationship('City', cascade='save-update, merge, delete',
+                          backref='state')
